a1cc833487cb025bf0ba156f6ce9d09c90f9bc99
OliverZim <oliver.zimmermann@student.hpi.de>
2023-08-28 11:10:13 +0200

    benchmarks for main server

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,4 +12,4 @@ if [ "$current_directory" == "scripts" ]; then
 fi
 
 #python train.py --wandb_run_name="finding_max_batch_size" --val_before_training=False --training_goal=2000 -d ./data/sw --batch_size_per_device=42  --workers=8 
-python train.py --val_before_training=False --training_goal=2000 -d ./data/sw --wandb_run_name="workers0" --workers=0
+python train.py --val_before_training=False --training_goal=200000 -d ./data/sw --wandb_run_name=oldVersion --compile=True --batch_size_per_device=42 --precision=16-mixed --force_deterministic=False --workers=1 --benchmark=True
