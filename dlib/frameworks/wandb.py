adae252131ca94a8531a371ba89a07e4a17a96d0
OliverZim <oliver.zimmermann@student.hpi.de>
2023-08-28 07:59:32 +0200

    set up wandb project for main server

--- dlib/frameworks/wandb.py
+++ dlib/frameworks/wandb.py
@@ -13,8 +13,8 @@ from loguru import logger
 
 from ..frameworks.pytorch import get_rank
 
-WANDB_PROJECT = "test-wandb"
-WANDB_ENTITY = "konstantinjdobler"
+WANDB_PROJECT = "main_server"
+WANDB_ENTITY = "nlp_benchmarks"
 
 if WANDB_ENTITY == "<your entity>" or WANDB_PROJECT == "<your project>":
     logger.warning(
